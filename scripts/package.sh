17a2516d042eb9097df94e77992675b7c6aec124
Kieran O'Neill <kieran.oneill@protonmail.com>
2021-01-06 11:38:29 +0000

    chore: updates the release and deployment process

--- scripts/package.sh
+++ scripts/package.sh
@@ -8,11 +8,6 @@ source ./scripts/set_vars.sh
 function main() {
   set_vars
 
-  if [ ! -d "${BUILD_PATH}" ]; then
-    printf "%b No \"build\" directory exists. Have you run \"./scripts.build.sh\"? \n" "${ERROR_PREFIX}"
-    exit 1
-  fi
-
   rm -rf "${DIST_PATH}"
   printf "%b Removed previous dist directory\n" "${INFO_PREFIX}"
 
@@ -21,7 +16,7 @@ function main() {
 
   # Zip up build.
   printf "%b Compressing theme... \n" "${INFO_PREFIX}"
-  zip -r -q "${DIST_PATH}"/fromthecornerofakitchentable.blog.zip "${BUILD_PATH}"/*
+  zip -r -q "${DIST_PATH}"/fromthecornerofakitchentable.blog.zip "${SRC_PATH}"/*
 
   printf "%b Done!\n" "${INFO_PREFIX}"
 }
--- scripts/set_vars.sh
+++ scripts/set_vars.sh
@@ -2,7 +2,6 @@
 
 function set_vars() {
   export BIN_PATH=node_modules/.bin
-  export BUILD_PATH=build
   export DIST_PATH=dist
   export ERROR_PREFIX='\033[0;31m[ERROR]\033[0m'
   export INFO_PREFIX='\033[1;33m[INFO]\033[0m'
